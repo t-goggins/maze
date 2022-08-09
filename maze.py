0c6ad8007d864a0d7e6086f7676f44305030c073
t-goggins <47161781+t-goggins@users.noreply.github.com>
2022-08-09 23:06:52 +0100

    Initial set up of pygame. Game loop / screen

--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
 import csv
+import pygame
 
 def load_map(filename):
     """Load map from external csv file into a variable, then return that 
@@ -37,9 +38,32 @@ class Player():
     #     self.x = x 
     #     self.y = y
 
-
-
 def main():
+    
+    pygame.init()
+    clock = pygame.time.Clock()
+
+    fps = 60
+
+    screen_w = 680
+    screen_h = 480
+    
+    screen = pygame.display.set_mode((screen_w, screen_h))
+    pygame.display.set_caption("Maze")
+
+    running = True
+    while running:
+        clock.tick(fps)
+
+        #Draw screen
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        #Update screen
+        pygame.display.update()
+
 
     # Create a map
     my_map = Maze_Map('map.csv')
